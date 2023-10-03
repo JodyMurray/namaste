57f481792f68dc29302e47f31126cf7ca1e4a51b
Jody M <jodyamm@hotmail.com>
2023-10-03 17:27:38 +0000

    Add: react-router-dom installed and component folder started

--- namaste/src/App.js
+++ namaste/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./components/Home";
+import Classes from "./components/Classes";
+import Instructors from "./components/Instructors";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={home} />
+          <Route path="/classes" component={Classes} />
+          <Route path="/instructors" component={Treknflow} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ namaste/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Yoga with Seána</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/classes">Classes</a>
+          </li>
+          <li>
+            <a href="/treknflow">Trek 'n Flow</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
