c00537cd5bbfc463198c6aa80b53728b3c08dae5
Jody M <jodyamm@hotmail.com>
2023-10-04 12:09:21 +0000

    Add: Favicon logo

--- namaste/src/App.js
+++ namaste/src/App.js
@@ -1,27 +1,25 @@
 import React from "react";
 import "./index.css";
 
-
 function App() {
   const header = (
-    <nav className="bg-blue-500 p-4 navbar">
-      <div className="container">
+    <nav className="p-4 navbar">
+      <div className="container mx-auto flex justify-between items-center">
         <a href="/" className="text-white text-2xl font-bold">
-          Yoga with Seána
         </a>
-        <ul className="flex space-x-4 nav-links">
+        <ul className="flex space-x-5 nav-links">
           <li>
-            <a href="/" className="text-white hover:text-blue-300">
+            <a href="/" className="text-white">
               Home
             </a>
           </li>
           <li>
-            <a href="/classes" className="text-white hover:text-blue-300">
+            <a href="/classes" className="text-white">
               Classes
             </a>
           </li>
           <li>
-            <a href="/trek-n-flow" className="text-white hover:text-blue-300">
+            <a href="/trek-n-flow" className="text-white">
               Trek 'n Flow
             </a>
           </li>
