b4f7a7489488e1b2c04218e2232ae706306fd855
Jody M <jodyamm@hotmail.com>
2023-10-06 07:06:27 +0000

    Install aos animations and add to homepage welcome message

--- namaste/src/components/header/Header.jsx
+++ namaste/src/components/header/Header.jsx
@@ -1,33 +1,40 @@
 import React from "react";
 import styles from "./Header.module.css";
+import logo from "../../assets/logo.png";
 
 function Header() {
   return (
-    <header className={`${styles.navbar} absolute top-0 left-0 right-0 bg-black bg-opacity-50 z-10`}>
+    <header
+      className={`${styles.navbar} absolute top-0 left-0 right-0 bg-black bg-opacity-50 z-10`}
+    >
       <nav className="p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <a href="/" className="text-white text-2xl font-bold">
-          <h2>Yoga with Seána</h2>
-        </a>
-        <ul className="flex space-x-6">
-          <li className={styles.NavLinks}>
-            <a href="/" className="text-white">
-              Home
-            </a>
-          </li>
-          <li className={styles.NavLinks}>
-            <a href="/classes" className="text-white">
-              Classes
-            </a>
-          </li>
-          <li className={styles.NavLinks}>
-            <a href="/trek-n-flow" className="text-white">
-              Trek 'n Flow
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+        <div className="container mx-auto flex justify-between items-center">
+          <a href="/" className="text-white text-2xl font-bold">
+            <img src={logo} className={styles.logo} alt="logo" height="40" />
+          </a>
+          <h1 className={`${styles.Heading1} text-white text-xl font-bold`}>
+            Yoga with Seána
+          </h1>{" "}
+          {/* Your heading */}
+          <ul className="flex space-x-6">
+            <li className={styles.NavLinks}>
+              <a href="/" className="text-white">
+                Home
+              </a>
+            </li>
+            <li className={styles.NavLinks}>
+              <a href="/classes" className="text-white">
+                Classes
+              </a>
+            </li>
+            <li className={styles.NavLinks}>
+              <a href="/trek-n-flow" className="text-white">
+                Trek 'n Flow
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 }
--- namaste/src/components/home/Home.jsx
+++ namaste/src/components/home/Home.jsx
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "aos/dist/aos.css";
+import AOS from "aos";
 import YogaHero from "../../assets/yogahero.jpeg";
-
+import styles from "./Home.module.css";
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000, // Animation duration in milliseconds
+      offset: 100, // Offset (in pixels) from the original trigger point
+      easing: "ease", // Easing for the animation (you can change this)
+    });
+  }, []);
   return (
     <div className="relative">
-      {/* Hero Image */}
       <img
         src={YogaHero}
         alt="Yoga Hero"
-        className="w-full h-screen object-cover"
+        className="w-full h-screen object-cover bg-cover bg-no-repeat bg-fixed"
       />
 
-      {/* Intro Message */}
       <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
         <div className="max-w-xl mx-auto text-white p-8">
-          <h1 className="text-4xl font-bold mb-4">Welcome to Yoga Bliss</h1>
-          <p className="text-lg">
-            Discover the path to inner peace and harmony through yoga. Join our
-            classes and start your journey to a healthier and more balanced
-            life.
+          <h1 className="text-4xl font-bold mb-4" data-aos="fade-right">
+            Welcome to Yoga with Seána
+          </h1>
+          <p className="text-lg" data-aos="fade-left">
+            Meet Seána, your dedicated Irish yoga teacher, guiding you through
+            the tranquil practice of Hatha Vinyasa on the sun-kissed shores of
+            Marbella. Discover the serenity of yoga by the beach as Seána offers
+            private yoga classes and specializes in nurturing expectant mothers
+            through soothing maternity yoga sessions. Embrace the harmonious
+            blend of nature, wellness, and mindfulness in each session with
+            Seána.
           </p>
-          <button className="bg-purple-500 hover:bg-purple-700 text-white font-semibold py-2 px-4 mt-4 rounded-full">
-            Get Started
+          <button
+            className="bg-purple-500 hover:bg-purple-700 text-white font-semibold py-2 px-4 mt-4 rounded-full"
+            data-aos="flip-up"
+          >
+            More Info
           </button>
         </div>
       </div>
