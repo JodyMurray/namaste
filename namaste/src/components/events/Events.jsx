8ac45c97095d869b43debdda7c70696ccd385b6e
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-10-07 09:16:44 +0200

    Feat: Import event hero image

--- namaste/src/components/events/Events.jsx
+++ namaste/src/components/events/Events.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import styles from "./Events.module.css";
+
+import treknflowhero from "../../assets/treknflow.jpg";
 
 export default function Events() {
   return (
-    <div className='mt-4'>Yoda youre a big poohead and smell like chips</div>
-  )
+    <>
+      <div>
+        <img
+          src={treknflowhero}
+          alt="Yoga class in mountains"
+          className={`${styles.HeroImage} w-full object-cover bg-cover bg-no-repeat bg-fixed`}
+        />
+      </div>
+    </>
+  );
 }
