e222e0d97df9e9a9d4c4baff64ef0e0279d910d1
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-10-08 17:09:24 +0200

    Feat: Prepare mock description typography

--- namaste/src/components/events/Events.jsx
+++ namaste/src/components/events/Events.jsx
@@ -86,9 +86,9 @@ export default function Events() {
       </div>
 
       <div>
-        <h1 className={``}>Title</h1>
-        <h2>. . .</h2>
-        <p>
+        <h1 className={`text-center mt-5 text-4xl`}>Title</h1>
+        <h2 className={`text-center text-4xl mb-4`}>. . .</h2>
+        <p className={`text-xl text-center`}>
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem
           accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
           ab illo inventore veritatis et quasi architecto beatae vitae dicta
@@ -97,6 +97,7 @@ export default function Events() {
           qui ratione voluptatem sequi nesciunt.
         </p>
 
+        <h2 className={`text-center text-4xl my-5`}>Photo Gallery</h2>
         <div class={`grid grid-cols-2 md:grid-cols-4 gap-4 ${styles.ImageMargin}`}>
           <div class="grid gap-3">
             <div>
