9b59a1d02dedcccbcdbf4546ea2048571cd767b1
Jody M <jodyamm@hotmail.com>
2023-10-10 08:13:30 +0000

    Add: AOS animation to all images across site

--- namaste/src/components/classes/Classes.jsx
+++ namaste/src/components/classes/Classes.jsx
@@ -49,30 +49,41 @@ const Classes = () => {
 
   return (
     <>
-      <div className="relative" data-aos="zoom-out">
-        <img
-          src={BeachYoga}
-          alt="Yoga Classes Background"
-          className="w-full h-screen object-cover"
-        />
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white bg-opacity-80 p-8 rounded-lg">
-          <h1 className="text-4xl font-semibold mb-4">Yoga Classes</h1>
-          <div className="bg-gray-200 bg-opacity-80 p-4 rounded-lg shadow-md">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
-              {classes.map((cls, index) => (
-                <div key={index} className="mb-4 flex flex-col">
-                  <h2 className="text-xl text-center border-b border-gray-400 font-semibold">
-                    {cls.day}
-                  </h2>
-                  <p className="text-gray-600">
-                    <span className="font-semibold">Location:</span>{" "}
-                    {cls.location}
-                  </p>
-                  <p className="text-gray-600">
-                    <span className="font-semibold">Time:</span> {cls.time}
-                  </p>
-                </div>
-              ))}
+      <div data-aos="zoom-out">
+        <div className="relative">
+          <img
+            src={BeachYoga}
+            alt="Yoga Classes Background"
+            className="w-full h-screen object-cover"
+          />
+        </div>
+        <div
+          className="absolute inset-0 flex items-center justify-center"
+          data-aos="zoom-in"
+          data-aos-easing="linear"
+          data-aos-duration="3000"
+        >
+          <div className="justify-center bg-opacity-80 p-8 rounded-lg">
+            <h1 className="text-4xl text-white font-semibold mb-4">
+              Yoga Classes
+            </h1>
+            <div className="bg-gray-200 bg-opacity-80 p-4 rounded-lg shadow-md">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+                {classes.map((cls, index) => (
+                  <div key={index} className="mb-4 flex flex-col">
+                    <h2 className="text-xl text-center border-b border-gray-400 font-semibold">
+                      {cls.day}
+                    </h2>
+                    <p className="text-gray-600">
+                      <span className="font-semibold">Location:</span>{" "}
+                      {cls.location}
+                    </p>
+                    <p className="text-gray-600">
+                      <span className="font-semibold">Time:</span> {cls.time}
+                    </p>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
@@ -83,6 +94,7 @@ const Classes = () => {
       <div className="bg-white-300c bg-opacity-80 p-8 rounded-lg relative">
         <div
           className={`${styles.Contact} border border-gray-400 rounded-lg overflow-hidden`}
+          data-aos="fade-zoom-out"
         >
           <div className="flex items-center">
             <div className="w-1/2 pr-8 m-10">
